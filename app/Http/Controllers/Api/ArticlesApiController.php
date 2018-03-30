b60b5fd3646c16c1b6f2d0ca902b3b1bab3e5e85
Liman Adamu Labaran <adamu.labaran@callenssolutions.com>
2018-03-30 23:25:22 +0000

    Get success

---
+++ app/Http/Controllers/Api/ArticlesApiController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+
+class ArticlesApiController extends Controller
+{
+    public function store() {
+        dd('success!');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\ArticlesApiController;
+use Api\ArticlesApiController;
 
 /*
 |--------------------------------------------------------------------------
