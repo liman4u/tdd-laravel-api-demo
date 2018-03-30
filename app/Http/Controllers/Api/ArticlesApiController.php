c56e9c97b0f09a42f915fc29bdf76fc8fc6db316
Liman Adamu Labaran <adamu.labaran@callenssolutions.com>
2018-03-30 23:30:10 +0000

    Add Request for Create Article

--- app/Http/Controllers/Api/ArticlesApiController.php
+++ app/Http/Controllers/Api/ArticlesApiController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
+use App\Articles\Requests\CreateArticleRequest;
 
 class ArticlesApiController extends Controller
 {
-    public function store() {
+    public function store(CreateArticleRequest $request) {
         dd('success!');
     }
 }
